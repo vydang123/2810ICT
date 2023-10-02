5faa49a3a227099a795c99d428e0214c10ce0d90
vydang123 <140564196+vydang123@users.noreply.github.com>
2023-10-02 16:05:47 +1000

    unit test and code for navigating

--- Python/test_navigating.py
+++ Python/test_navigating.py
@@ -1,29 +1,28 @@
-import pytest
 import wx
 from homepage import MainPage
 
-# Since we don't have the actual implementations for methods like show_View_Case_Penalty, show_Offence_code, etc.,
-# we'll mock them for the purpose of this test. In a real environment, you would not need these mock implementations.
+def test_switch_to_page():
+    app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window
+    frame = MainPage(None)  # Create a frame
 
-def mock_show_function():
-    pass
+    # Test navigation to "View_Case_Penalty"
+    frame.switch_to_page("View_Case_Penalty")
+    assert hasattr(frame, 'active_panel'), "Failed to navigate to View_Case_Penalty"
 
+    # Test navigation to "Offence_code"
+    frame.switch_to_page("Offence_code")
+    assert hasattr(frame, 'active_panel'), "Failed to navigate to Offence_code"
 
-setattr(MainPage, "show_View_Case_Penalty", mock_show_function)
-setattr(MainPage, "show_Offence_code", mock_show_function)
-setattr(MainPage, "show_radar_camera_cases", mock_show_function)
-setattr(MainPage, "show_Mobile_Offence_code", mock_show_function)
-setattr(MainPage, "show_Seatbelt_Offence_code", mock_show_function)
+    # Test navigation to "Radar/Camera Cases"
+    frame.switch_to_page("Radar/Camera Cases")
+    assert hasattr(frame, 'active_panel'), "Failed to navigate to Radar/Camera Cases"
 
+    # Test navigation to "Mobile Phone Usage"
+    frame.switch_to_page("Mobile Phone Usage")
+    assert hasattr(frame, 'active_panel'), "Failed to navigate to Mobile Phone Usage"
 
-@pytest.mark.parametrize("page_name", ["View_Case_Penalty", "Offence_code", "Radar/Camera Cases", "Mobile Phone Usage",
-                                       "Seatbelt not fastened"])
-def test_switch_to_page(page_name):
-    app = wx.App(False)
-    frame = MainPage(None)
+    # Test navigation to "Seatbelt not fastened"
+    frame.switch_to_page("Seatbelt not fastened")
+    assert hasattr(frame, 'active_panel'), "Failed to navigate to Seatbelt not fastened"
 
-    frame.switch_to_page(page_name)
-
-    # For simplicity, we'll just check if the active_panel attribute of MainPage is not None.
-    # In a real test, you would check specific components related to the given page_name to ensure they are displayed correctly.
-    assert frame.active_panel is not None, f"Failed to switch to {page_name} page"
+    frame.Destroy()  # Cleanup
